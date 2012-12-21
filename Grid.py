5f50c9a822d79c01c207c0503d27f5586c13adaa
Alex Cappiello <acappiel@andrew.cmu.edu>
2012-12-21 03:40:21 -0500

    Added reconstruction of the files.
    Signed-off-by: Alex Cappiello <acappiel@andrew.cmu.edu>

--- Grid.py
+++ Grid.py
@@ -4,28 +4,16 @@ from PIL import Image
 class Grid:
     """Holds all of the information and logic for moving data between the
     joined pngs and the individual block pngs."""
-    def __init__ (obj, img_path, mapping, workdir, res = [16], \
+    def __init__ (obj, img_path, mapping, workdir, \
                       grid_width = 16, grid_height = 16):
         obj.img_path = img_path
         obj.map = mapping
         obj.workdir = workdir
-        obj.res = res
         obj.grid_width = grid_width
         obj.grid_height = grid_height
 
-    def write_block_to_file (obj, x, y, block):
-        """Extract the raw data for an individual block in the source image
-        and save it to a file."""
-
-        # Something seems to be getting flipped around here.
-        # Works now, but unsure what exactly was wrong.
-        tlx = x * obj.block_size
-        tly = y * obj.block_size
-        brx = tlx + (block.height * obj.block_size)
-        bry = tly + (block.width * obj.block_size)
-        block_img = obj.source.crop((tly, tlx, bry, brx))
-
-        # Of form 'xx_yy_name.png'.
+    def construct_filename (obj, x, y, block):
+        """Of form 'xx_yy_name.png'."""
         filename = obj.workdir + os.sep
         if (x < 10):
             filename = filename + "0"
@@ -33,6 +21,25 @@ class Grid:
         if (y < 10):
             filename = filename + "0"
         filename = filename + str(y) + "_" + block.name + ".png"
+        return filename
+
+    def construct_location (obj, x, y, block):
+        """Find the location of a block in the combined image."""
+        # Something seems to be getting flipped around here.
+        # Works now, but unsure what exactly was wrong.
+        # PIL takes inputs backwards sometimes.
+        tlx = x * obj.block_size
+        tly = y * obj.block_size
+        brx = tlx + (block.height * obj.block_size)
+        bry = tly + (block.width * obj.block_size)
+        return (tly, tlx, bry, brx)
+
+    def write_block_to_file (obj, x, y, block):
+        """Extract the raw data for an individual block in the source image
+        and save it to a file."""
+        block_img = obj.source.crop(obj.construct_location(x, y, block))
+
+        filename = obj.construct_filename(x, y, block)
 
         block_img.save(filename)
 
@@ -60,3 +67,61 @@ class Grid:
                 if (i, j) in obj.map:
                     block = obj.map[(i, j)]
                     obj.write_block_to_file(i, j, block)
+
+    def read_and_paste_block (obj, x, y, block):
+        """Read a block from file and place it in the grid.
+        Blocks are scaled as needed."""
+        filename = obj.construct_filename(x, y, block)
+
+        # Some checks on inputs.
+        if (not os.path.exists(filename)):
+            utils.raise_error("Could not open: " + filenema)
+
+        block_img = Image.open(filename)
+        (width, height) = block_img.size
+        if (width / block.width != height / block.height):
+            utils.raise_error("Invalid block proportion: " + filename)
+
+        if ((width / block.width) != obj.block_size):
+            new_width = obj.block_size * block.width
+            new_height = obj.block_size * block.height
+            block_img = block_img.resize((new_width, new_height), \
+                                             Image.BICUBIC)
+
+        obj.output.paste(block_img, obj.construct_location(x, y, block))
+
+    def reconstruct (obj, res = [16]):
+        """Assemble the grid from the individual block files.
+        If more than one resolution is desired, the largest resolution is used
+        and the end result is scaled down. The output files will have
+        the size denoted in the filename."""
+        res = list(set(res)) # Squish duplicates.
+        if (len(res) == 0):
+            utils.raise_error("Empty output.")
+        res.sort(reverse=True)
+        obj.block_size = res.pop(0)
+
+        obj.output = Image.new("RGBA", (obj.block_size * obj.grid_width, \
+                                            obj.block_size * obj.grid_height), \
+                                   (0, 0, 0, 0))
+
+        # Loop over everything, ignoring empty spaces.
+        for i in xrange(obj.grid_width):
+            for j in xrange(obj.grid_height):
+                if (i, j) in obj.map:
+                    block = obj.map[(i, j)]
+                    obj.read_and_paste_block(i, j, block)
+
+        # Save the primary output size.
+        ext = "_" + str(obj.block_size) + "x" + str(obj.block_size) + ".png"
+        outfile = obj.img_path.replace(".png", ext)
+        obj.output.save(outfile)
+
+        # Save additional output sizes.
+        for size in res:
+            ext = "_" + str(size) + "x" + str(size) + ".png"
+            outfile = obj.img_path.replace(".png", ext)
+            dims = (size * obj.grid_width, size * obj.grid_height)
+            img = obj.output.resize(dims, Image.BICUBIC)
+            img.save(outfile)
+
