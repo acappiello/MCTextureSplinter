8c17721f94221d1bd7084e9ad01321cb615580bc
Alex Cappiello <acappiel@andrew.cmu.edu>
2012-12-24 01:23:19 -0500

    Docstrings and minor changes to the GUI.
    Signed-off-by: Alex Cappiello <acappiel@andrew.cmu.edu>

--- TkGUI.py
+++ TkGUI.py
@@ -2,96 +2,111 @@
 # Date: 12/21/12
 # Updated: 12/21/12
 
-import tkFileDialog, Grid, os
+import tkFileDialog, TextureGrid, os
 from terrain import terrain_map
 from Tkinter import *
 
 class CallerInfo:
-    def __init__ (obj):
-        pass
+    """Collects all information needed to build a TextureGrid.Grid object for
+    each file we work with."""
+    def __init__ (self, mapping):
+        """Initially, only the mapping is needed. Additional information is
+        added as needed."""
+        self.map = mapping
 
 class Gui:
-    def launch_deconstruct (obj):
-        terrain_path = obj.terrain_path.get()
-        if (terrain_path == ""):
-            terrain_path = "terrain.png"
-        working_path = obj.working_path.get()
+    """Everything needed for the Tkinter GUI."""
+    def make_grid (self, item, entry):
+        """Construct a TextureGrid.Grid object by pulling information from the
+        GUI."""
+        working_path = self.working_path.get()
         if (working_path == ""):
             working_path = os.getcwd()
 
-        if (obj.run_terrain):
-            terrain = Grid.Grid(terrain_path, terrain_map, \
-                                    working_path + "/terrain")
-            terrain.deconstruct()
+        path = entry.path.get()
+        if (path == ""):
+            path = item
+        subdir = os.sep + os.path.splitext(item)[0]
+        return TextureGrid.Grid(path, entry.map, working_path + subdir)
 
-    def launch_reconstruct (obj):
-        terrain_path = obj.terrain_path.get()
-        working_path = obj.working_path.get()
+    def launch_deconstruct (self):
+        """Loop over all files, calling deconstruct."""
+        for (item, entry) in self.texture_parts.items():
+            if (entry.state.get()):
+                grid = self.make_grid(item, entry)
+                grid.deconstruct()
+
+    def launch_reconstruct (self):
+        """Loop over all files, calling reconstruct."""
+        working_path = self.working_path.get()
         if (working_path == ""):
             working_path = os.getcwd()
-        terrain = Grid.Grid(terrain_path, terrain_map, \
-                                working_path + "/terrain")
-        resolutions = []
-        for (res, state) in obj.states:
-            if (state.get()):
-                resolutions.append(res)
-        terrain.reconstruct(resolutions)
+        for (item, entry) in self.texture_parts.items():
+            if (entry.state.get()):
+                grid = self.make_grid(item, entry)
+                resolutions = []
+                for (res, state) in self.res_options:
+                    if (state.get()):
+                        resolutions.append(res)
+                grid.reconstruct(resolutions)
 
     @staticmethod
     def browse_for_file (e):
+        """Pull up a file browsing dialog and dump the resulting path into the
+        Tkinter.Entry widget e."""
         path = tkFileDialog.askopenfilename(filetypes=[("PNG", ".png")])
-        if (path != ""):
+        if (path != ""): # Unless they hit cancel.
             e.delete(0, END)
             e.insert(0, path)
 
     @staticmethod
     def browse_for_folder (e):
+        """Pull up a folder browsing dialog and dump the resulting path into the
+        Tkinter.Entry widget e."""
         path = tkFileDialog.askdirectory()
-        if (path != ""):
+        if (path != ""): # Unless they hit cancel.
             e.delete(0, END)
             e.insert(0, path)
 
-    def init_dict (obj):
+    def init_dict (self):
+        """All texture pack files to be split at added here."""
         d = dict()
-        d["terrain.png"] = CallerInfo()
-        d["items.png"] = CallerInfo()
-        obj.texture_parts = d
+        d["terrain.png"] = CallerInfo(terrain_map)
+        d["items.png"] = CallerInfo(None)
+        self.texture_parts = d
 
-    def init_path_inputs (obj):
-        Label(obj.canvas, text="Location of terrain.png:").pack(anchor="w")
-        terrain = Frame(obj.canvas)
-        terrain_path = Entry(terrain, width=80)
-        terrain_path.pack(side=LEFT)
-        obj.terrain_path = terrain_path
-        terrain_browse = Button(terrain, text="Browse",
-                                        command=lambda:
-                                            Gui.browse_for_file(terrain_path))
-        terrain_browse.pack(side=LEFT)
-        terrain.pack()
+    def init_path_inputs (self):
+        """Add an Entry field to the GUI for each file and one for the working
+        directory. Each is in its own Frame with a browse Button."""
+        # Need this to evaluate input argument immediately.
+        # Faking a curried function. Thanks 15-150.
+        browse_wrapper = lambda v: lambda: Gui.browse_for_file(v)
 
-        Label(obj.canvas, text="Location of items.png:").pack(anchor="w")
-        items = Frame(obj.canvas)
-        items_path = Entry(items, width=80)
-        items_path.pack(side=LEFT)
-        obj.items_path = items_path
-        items_browse = Button(items, text="Browse",
-                                      command=lambda:
-                                          Gui.browse_for_file(items_path))
-        items_browse.pack(side=LEFT)
-        items.pack()
+        for item in self.texture_parts.keys():
+            Label(self.canvas, text="Location of %s:"%item).pack(anchor="w")
+            container = Frame(self.canvas)
+            path = Entry(container, width=80)
+            path.pack(side=LEFT)
+            cmd = browse_wrapper(path)
+            self.texture_parts[item].path = path
+            browse = Button(container, text="Browse",
+                            command=cmd)
+            browse.pack(side=LEFT)
+            container.pack()
 
-        Label(obj.canvas, text="Working directory:").pack(anchor="w")
-        working = Frame(obj.canvas)
+        Label(self.canvas, text="Working directory:").pack(anchor="w")
+        working = Frame(self.canvas)
         working_path = Entry(working, width=80)
         working_path.pack(side=LEFT)
-        obj.working_path = working_path
+        self.working_path = working_path
         working_browse = Button(working, text="Browse",
                                         command=lambda:
                                             Gui.browse_for_folder(working_path))
         working_browse.pack(side=LEFT)
         working.pack()
 
-    def init_res_checkboxes (obj, parent):
+    def init_res_checkboxes (self, parent):
+        """Add checkboxes to the GUI for each possible output resolution."""
         resolutions = [16, 32, 64, 128, 256, 512, 1024]
         checkboxes = Frame(parent)
         Label(checkboxes, text="Resolutions:").pack()
@@ -102,55 +117,59 @@ class Gui:
             states.append((res, state))
             box = Checkbutton(checkboxes, text=label, variable=state)
             box.pack(anchor="w")
-        obj.states = states
+        self.res_options = states
         checkboxes.pack(side=LEFT)
 
-    def init_parts (obj, parent):
+    def init_parts (self, parent):
+        """Add checkboxes to the GUI for each file to decide whether to process
+        it. All are initially enabled."""
         parts_box = Frame(parent)
         Label(parts_box, text="Files to process:").pack()
-        obj.run_terrain = BooleanVar()
-        terrain = Checkbutton(parts_box, text="terrain.png", \
-                                  variable = obj.run_terrain)
-        terrain.select()
-        terrain.pack(anchor="w")
-        obj.run_items = BooleanVar()
-        items = Checkbutton(parts_box, text="items.png", variable=obj.run_items)
-        items.select()
-        items.pack(anchor="w")
+        for item in self.texture_parts.keys():
+            state = BooleanVar()
+            button = Checkbutton(parts_box, text=item, variable=state)
+            button.select()
+            button.pack(anchor="w")
+            self.texture_parts[item].state = state
+
         parts_box.pack()
 
-    def init_options (obj):
-        opt_box = Frame(obj.canvas)
-        obj.init_res_checkboxes(opt_box)
-        obj.init_parts(opt_box)
+    def init_options (self):
+        """Wrapper for calling everything in the Frame for various options."""
+        opt_box = Frame(self.canvas)
+        self.init_res_checkboxes(opt_box)
+        self.init_parts(opt_box)
         opt_box.pack(anchor="w")
 
-    def build (obj):
-        obj.init_dict()
-        obj.init_path_inputs()
-        obj.init_options()
+    def build (self):
+        """Wrapper for calling init functions for various other pieces of the
+        GUI."""
+        self.init_dict()
+        self.init_path_inputs()
+        self.init_options()
 
-        deconstruct = Button(obj.canvas, text="Deconstruct",
-                                     command=lambda: obj.launch_deconstruct())
+        deconstruct = Button(self.canvas, text="Deconstruct",
+                                     command=lambda: self.launch_deconstruct())
         deconstruct.pack(side=LEFT)
-        reconstruct = Button(obj.canvas, text="Reconstruct",
-                                     command=lambda: obj.launch_reconstruct())
+        reconstruct = Button(self.canvas, text="Reconstruct",
+                                     command=lambda: self.launch_reconstruct())
         reconstruct.pack(side=LEFT)
 
-    def __init__ (obj):
+    def __init__ (self):
+        """Create the root and canvas. Then, build the GUI and run."""
         root = Tk()
 
         window_width = 800
         window_height = 600
 
-        obj.canvas = Canvas(root)#, width=window_width, height=window_height)
-        obj.canvas.pack()
+        self.canvas = Canvas(root)#, width=window_width, height=window_height)
+        self.canvas.pack()
         #root.resizable(width=0, height=0)
 
-        obj.window_width = window_width
-        obj.window_height = window_height
+        self.window_width = window_width
+        self.window_height = window_height
 
-        obj.build()
+        self.build()
         # set up events
         #root.bind("<Key>", lambda e: keyPressed(e, canvas))
         #timerFired(canvas, speedMultiplier)
