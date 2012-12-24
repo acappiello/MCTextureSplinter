8f59d39ec4c86aa49635042222d2a85e953dad56
Alex Cappiello <acappiel@andrew.cmu.edu>
2012-12-23 23:38:20 -0500

    About to rewrite GUI using dictionary.
    Signed-off-by: Alex Cappiello <acappiel@andrew.cmu.edu>

--- TkGUI.py
+++ TkGUI.py
@@ -2,8 +2,13 @@
 # Date: 12/21/12
 # Updated: 12/21/12
 
-import Tkinter, tkFileDialog, Grid, os
+import tkFileDialog, Grid, os
 from terrain import terrain_map
+from Tkinter import *
+
+class CallerInfo:
+    def __init__ (obj):
+        pass
 
 class Gui:
     def launch_deconstruct (obj):
@@ -13,9 +18,11 @@ class Gui:
         working_path = obj.working_path.get()
         if (working_path == ""):
             working_path = os.getcwd()
-        terrain = Grid.Grid(terrain_path, terrain_map, \
-                                working_path + "/terrain")
-        terrain.deconstruct()
+
+        if (obj.run_terrain):
+            terrain = Grid.Grid(terrain_path, terrain_map, \
+                                    working_path + "/terrain")
+            terrain.deconstruct()
 
     def launch_reconstruct (obj):
         terrain_path = obj.terrain_path.get()
@@ -34,81 +41,109 @@ class Gui:
     def browse_for_file (e):
         path = tkFileDialog.askopenfilename(filetypes=[("PNG", ".png")])
         if (path != ""):
-            e.delete(0, Tkinter.END)
+            e.delete(0, END)
             e.insert(0, path)
 
     @staticmethod
     def browse_for_folder (e):
         path = tkFileDialog.askdirectory()
         if (path != ""):
-            e.delete(0, Tkinter.END)
+            e.delete(0, END)
             e.insert(0, path)
 
+    def init_dict (obj):
+        d = dict()
+        d["terrain.png"] = CallerInfo()
+        d["items.png"] = CallerInfo()
+        obj.texture_parts = d
+
     def init_path_inputs (obj):
-        Tkinter.Label(obj.canvas, text="Location of terrain.png:").pack(anchor="w")
-        terrain = Tkinter.Frame(obj.canvas)
-        terrain_path = Tkinter.Entry(terrain, width=80)
-        terrain_path.pack(side=Tkinter.LEFT)
+        Label(obj.canvas, text="Location of terrain.png:").pack(anchor="w")
+        terrain = Frame(obj.canvas)
+        terrain_path = Entry(terrain, width=80)
+        terrain_path.pack(side=LEFT)
         obj.terrain_path = terrain_path
-        terrain_browse = Tkinter.Button(terrain, text="Browse",
+        terrain_browse = Button(terrain, text="Browse",
                                         command=lambda:
                                             Gui.browse_for_file(terrain_path))
-        terrain_browse.pack(side=Tkinter.LEFT)
+        terrain_browse.pack(side=LEFT)
         terrain.pack()
 
-        Tkinter.Label(obj.canvas, text="Location of items.png:").pack(anchor="w")
-        items = Tkinter.Frame(obj.canvas)
-        items_path = Tkinter.Entry(items, width=80)
-        items_path.pack(side=Tkinter.LEFT)
+        Label(obj.canvas, text="Location of items.png:").pack(anchor="w")
+        items = Frame(obj.canvas)
+        items_path = Entry(items, width=80)
+        items_path.pack(side=LEFT)
         obj.items_path = items_path
-        items_browse = Tkinter.Button(items, text="Browse",
+        items_browse = Button(items, text="Browse",
                                       command=lambda:
                                           Gui.browse_for_file(items_path))
-        items_browse.pack(side=Tkinter.LEFT)
+        items_browse.pack(side=LEFT)
         items.pack()
 
-        Tkinter.Label(obj.canvas, text="Working directory:").pack(anchor="w")
-        working = Tkinter.Frame(obj.canvas)
-        working_path = Tkinter.Entry(working, width=80)
-        working_path.pack(side=Tkinter.LEFT)
+        Label(obj.canvas, text="Working directory:").pack(anchor="w")
+        working = Frame(obj.canvas)
+        working_path = Entry(working, width=80)
+        working_path.pack(side=LEFT)
         obj.working_path = working_path
-        working_browse = Tkinter.Button(working, text="Browse",
+        working_browse = Button(working, text="Browse",
                                         command=lambda:
                                             Gui.browse_for_folder(working_path))
-        working_browse.pack(side=Tkinter.LEFT)
+        working_browse.pack(side=LEFT)
         working.pack()
 
-    def init_res_checkboxes (obj):
+    def init_res_checkboxes (obj, parent):
         resolutions = [16, 32, 64, 128, 256, 512, 1024]
-        checkboxes = Tkinter.Frame(obj.canvas)
+        checkboxes = Frame(parent)
+        Label(checkboxes, text="Resolutions:").pack()
         states = []
         for res in resolutions:
             label = str(res) + "x" + str(res)
-            state = Tkinter.IntVar()
+            state = IntVar()
             states.append((res, state))
-            box = Tkinter.Checkbutton(checkboxes, text=label, variable=state)
-            box.pack(side=Tkinter.LEFT)
+            box = Checkbutton(checkboxes, text=label, variable=state)
+            box.pack(anchor="w")
         obj.states = states
-        checkboxes.pack()
+        checkboxes.pack(side=LEFT)
+
+    def init_parts (obj, parent):
+        parts_box = Frame(parent)
+        Label(parts_box, text="Files to process:").pack()
+        obj.run_terrain = BooleanVar()
+        terrain = Checkbutton(parts_box, text="terrain.png", \
+                                  variable = obj.run_terrain)
+        terrain.select()
+        terrain.pack(anchor="w")
+        obj.run_items = BooleanVar()
+        items = Checkbutton(parts_box, text="items.png", variable=obj.run_items)
+        items.select()
+        items.pack(anchor="w")
+        parts_box.pack()
+
+    def init_options (obj):
+        opt_box = Frame(obj.canvas)
+        obj.init_res_checkboxes(opt_box)
+        obj.init_parts(opt_box)
+        opt_box.pack(anchor="w")
 
     def build (obj):
+        obj.init_dict()
         obj.init_path_inputs()
-        obj.init_res_checkboxes()
+        obj.init_options()
 
-        deconstruct = Tkinter.Button(obj.canvas, text="Deconstruct",
+        deconstruct = Button(obj.canvas, text="Deconstruct",
                                      command=lambda: obj.launch_deconstruct())
-        deconstruct.pack(side=Tkinter.LEFT)
-        reconstruct = Tkinter.Button(obj.canvas, text="Reconstruct",
+        deconstruct.pack(side=LEFT)
+        reconstruct = Button(obj.canvas, text="Reconstruct",
                                      command=lambda: obj.launch_reconstruct())
-        reconstruct.pack(side=Tkinter.LEFT)
+        reconstruct.pack(side=LEFT)
 
     def __init__ (obj):
-        root = Tkinter.Tk()
+        root = Tk()
 
         window_width = 800
         window_height = 600
 
-        obj.canvas = Tkinter.Canvas(root)#, width=window_width, height=window_height)
+        obj.canvas = Canvas(root)#, width=window_width, height=window_height)
         obj.canvas.pack()
         #root.resizable(width=0, height=0)
 
