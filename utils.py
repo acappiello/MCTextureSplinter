18df60a1d1277bc7696f5e789e77bacc0a83dbe8
Alex Cappiello <acappiel@andrew.cmu.edu>
2012-12-28 06:03:04 -0500

    Error message also spawns a message box window.
    Signed-off-by: Alex Cappiello <acappiel@andrew.cmu.edu>

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 """Misc. utilities for MCTextureSplinter."""
 
 import sys
+import Tkinter
 import tkMessageBox
 
 __author__ = "Alex Cappiello"
@@ -10,5 +11,10 @@ __license__ = "See LICENSE.txt"
 def raise_error(msg):
     """Handle when things go wrong in some reasonable way.
     For now, abort."""
-    print "Error: " + msg
-    sys.exit(1)
+    print "Error: %s" % (msg)
+    try:
+        root = Tkinter.Tk()
+        root.withdraw()
+        tkMessageBox.showerror("Error", msg)
+    finally:
+        sys.exit(1)
